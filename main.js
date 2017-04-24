9a23bc1ffc1c3ce5c31d27eddf3e3ca8e4819524
jmrigali <jmrigali@ymail.com>
2017-04-24 09:46:54 -0600

    totals figured out

--- main.js
+++ main.js
@@ -4,6 +4,39 @@ $(document).ready(function() {
 
  $(".button-collapse").sideNav();
 
- 
+var $card = $('.card');
+var $subTotal = 0;
+
+
+
+
+
+for(let i = 0; i < $card.length; i++) {
+   var $button = $($card[i]).find('button')[0];
+
+
+  $($button).click('button', function () {
+    var $something = $($card[i]).children('.card-content');
+    var $li= $('<li style="font-size:0.5em;">');
+
+    $item = $($something).children('h5').text();
+    $price = $($something).children('p').children('span').text();
+    $subTotal += parseFloat($price);
+
+
+    $li.text($item);
+
+    var $tax = $subTotal * 0.0885;
+    var $total = $tax + $subTotal;
+
+    $("#items").append($li);
+    $("#subtotal").text($subTotal.toFixed(2));
+    $("#tax").text($tax.toFixed(2));
+    $("#total").text($total.toFixed(2));
+  });
+}
+
+
+
 
 });
